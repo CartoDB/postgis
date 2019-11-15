960b61757b77bba4344850a9b8ad6f9355c32d5c
manmorjim <manmorjim@gmail.com>
2019-11-15 16:10:04 +0100

    Using pstrdup to copy strings

--- libpgcommon/lwgeom_transform.c
+++ libpgcommon/lwgeom_transform.c
@@ -428,7 +428,7 @@ GetProjStringsSPI(int32_t srid)
 			static const char *proj4text_4326 = "+proj=longlat +datum=WGS84 +no_defs";
 			static const char *projt4text_3857 =
 			    "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs";
-			strncpy(strs.proj4text, srid == 4326 ? proj4text_4326 : projt4text_3857, maxprojlen - 1);
+			strs.proj4text = pstrdup(srid == 4326 ? proj4text_4326 : projt4text_3857);
 		}
 		else
 		{
